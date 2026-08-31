270d52f6e22905492f90dfe56cc081e60b5953a2
Omnia Tawfek <omniatawfek@std.mans.edu.eg>
2026-08-31 23:47:37 +0300

    building the layout and pages

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,11 @@
+export const metadata = {
+  title: "Guest area",
+};
+
 export default function Page() {
-  return <div>Your Account</div>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome,jonas
+    </h2>
+  );
 }
